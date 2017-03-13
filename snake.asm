6f62dd119381f85f4b39134460f2cfae4fedbc48
Steven Milov <sjm141@pitt.edu>
2017-03-13 17:25:08 -0400

    Add files via upload

---
+++ snake.asm
@@ -0,0 +1,109 @@
+# Steven Milov, Andrew DiFurio, and a tiny bit of Craig Brust
+
+# Computer Organization and Assembly Language
+
+# T,TH 1:00 PM
+
+.text
+main:
+	
+
+
+
+
+
+
+
+	###################### FUNCTIONS #############################################
+  # void _setLED(int x, int y, int color)
+	#   sets the LED at (x,y) to color
+	#   color: 0=off, 1=red, 2=yellow, 3=green
+	#
+	# arguments: $a0 is x, $a1 is y, $a2 is color
+	# trashes:   $t0-$t3
+	# returns:   none
+	##
+_setLED: #######################################################################
+
+	#ADDING TO STACK - MODIFIED BY STEVEN
+
+	addi $sp, $sp, -16
+	sw $t0, 0($sp)
+	sw $t1, 4($sp)
+	sw $t2, 8($sp)
+	sw $t3, 12($sp)
+
+	# byte offset into display = y * 16 bytes + (x / 4)
+	sll	$t0,$a1,4      # y * 16 bytes
+	srl	$t1,$a0,2      # x / 4
+	add	$t0,$t0,$t1    # byte offset into display
+	li	$t2,0xffff0008 # base address of LED display
+	add	$t0,$t2,$t0    # address of byte with the LED
+	# now, compute led position in the byte and the mask for it
+	andi	$t1,$a0,0x3    # remainder is led position in byte
+	neg	$t1,$t1        # negate position for subtraction
+	addi	$t1,$t1,3      # bit positions in reverse order
+	sll	$t1,$t1,1      # led is 2 bits
+	# compute two masks: one to clear field, one to set new color
+	li	$t2,3
+	sllv	$t2,$t2,$t1
+	not	$t2,$t2        # bit mask for clearing current color
+	sllv	$t1,$a2,$t1    # bit mask for setting color
+	# get current LED value, set the new field, store it back to LED
+	lbu	$t3,0($t0)     # read current LED value
+	and	$t3,$t3,$t2    # clear the field for the color
+	or	$t3,$t3,$t1    # set color field
+	sb	$t3,0($t0)     # update display
+
+	#REMOVING FROM STACK - MODIFIED BY STEVEN
+
+	lw $t0, 0($sp)
+	lw $t1, 4($sp)
+	lw $t2, 8($sp)
+	lw $t3, 12($sp)
+	addi $sp,$sp,16
+
+	jr	$ra
+	##############################################################################
+
+	##
+	# int _getLED(int x, int y)
+	#   returns the value of the LED at position (x,y)
+	#
+	#  arguments: $a0 holds x, $a1 holds y
+	#  trashes:   $t0-$t2
+	#  returns:   $v0 holds the value of the LED (0, 1, 2 or 3)
+	##
+_getLED: #######################################################################
+	#ADDING TO STACK - MODIFIED BY STEVEN
+
+	addi $sp, $sp, -12
+	sw $t0, 0($sp)
+	sw $t1, 4($sp)
+	sw $t2, 8($sp)
+
+	# byte offset into display = y * 16 bytes + (x / 4)
+	sll  $t0,$a1,4      # y * 16 bytes
+	srl  $t1,$a0,2      # x / 4
+	add  $t0,$t0,$t1    # byte offset into display
+	la   $t2,0xffff0008
+	add  $t0,$t2,$t0    # address of byte with the LED
+	# now, compute bit position in the byte and the mask for it
+	andi $t1,$a0,0x3    # remainder is bit position in byte
+	neg  $t1,$t1        # negate position for subtraction
+	addi $t1,$t1,3      # bit positions in reverse order
+    	sll  $t1,$t1,1      # led is 2 bits
+	# load LED value, get the desired bit in the loaded byte
+	lbu  $t2,0($t0)
+	srlv $t2,$t2,$t1    # shift LED value to lsb position
+	andi $v0,$t2,0x3    # mask off any remaining upper bits
+
+	#REMOVING FROM STACK - MODIFIED BY STEVEN
+
+	lw $t0, 0($sp)
+	lw $t1, 4($sp)
+	lw $t2, 8($sp)
+	addi $sp,$sp,12
+
+	jr   $ra
+	##############################################################################
